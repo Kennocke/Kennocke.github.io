5438148cb8a8f1826afef13ef8eaaaf67168b4aa
Kennocke <32141916+Kennocke@users.noreply.github.com>
2020-01-19 18:21:27 +0300

    Добавил ответ на билет

--- scripts/mainjs.js
+++ scripts/mainjs.js
@@ -4,8 +4,9 @@ function lab1() {
 	but_elem[0].style.fontWeight='bold';
 	but_elem[1].style.fontWeight='normal';
 	but_elem[2].style.fontWeight='normal';
+	but_elem[3].style.fontWeight='normal';
 	elem[0].setAttribute("src", "pages/lab1.html");
-	elem[0].style.height = "2900px";
+	elem[0].style.height = "2700px";
 	elem[0].style.marginBottom = "50px";
 };
 
@@ -15,6 +16,7 @@ function lab2() {
 	but_elem[1].style.fontWeight='bold';
 	but_elem[0].style.fontWeight='normal';
 	but_elem[2].style.fontWeight='normal';
+	but_elem[3].style.fontWeight='normal';
 	elem[0].setAttribute("src", "pages/lab2.html");
 	elem[0].style.height = "1200px";
 	elem[0].style.marginBottom = "50px";
@@ -26,7 +28,20 @@ function lab3() {
 	but_elem[2].style.fontWeight='bold';
 	but_elem[0].style.fontWeight='normal';
 	but_elem[1].style.fontWeight='normal';
+	but_elem[3].style.fontWeight='normal';
 	elem[0].setAttribute("src", "pages/lab3.html");
 	elem[0].style.height = "14500px";
+	elem[0].style.marginBottom = "50px";
+};
+
+function bilet() {
+	var but_elem = document.getElementsByClassName("button");
+	var elem = document.getElementsByTagName("iframe");
+	but_elem[3].style.fontWeight='bold';
+	but_elem[0].style.fontWeight='normal';
+	but_elem[1].style.fontWeight='normal';
+	but_elem[2].style.fontWeight='normal';
+	elem[0].setAttribute("src", "pages/bilet.html");
+	elem[0].style.height = "5500px";
 	elem[0].style.marginBottom = "50px";
 };
